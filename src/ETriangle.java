2d079a57a09a51801285548bdffd7b352582292a
BT_1N3_19 <jlin3392@bths.edu>
2018-10-16 14:43:17 -0400

    ETriangle

---
+++ src/ETriangle.java
@@ -0,0 +1,21 @@
+public class ETriangle extends Shape
+{
+    private int side;
+
+    public ETriangle(int side)
+    {
+        super("ETriangle");
+        this.side = side;
+    }
+    @Override
+    public double getArea()
+    {
+        return (Math.sqrt(3)/4*Math.pow(side,2));
+    }
+
+    @Override
+    public double getPerimeter()
+    {
+        return side*3;
+    }
+}
