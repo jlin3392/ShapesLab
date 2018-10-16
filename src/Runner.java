fd3a553c2ae775691779e004a7bdfba36eb4a2d1
BT_1N3_20 <wtsang1931@bths.edu>
2018-10-16 14:31:52 -0400

    circle

--- src/Runner.java
+++ src/Runner.java
@@ -1,5 +1,18 @@
-public class Runner {
-    public static void main(String[] args) {
+public class Runner
+{
+    public static void main(String[] args)
+    {
+        Shape shape1 = new Rectangle(, );
+        Shape shape2 = new ETriangle();
+        Shape shape3 = new Circle();
 
+        Shape[] shapes = {shape1, shape2, shape3};
+
+        for (Shape cur : shapes)
+        {
+            System.out.println(cur);
+            System.out.println("My area is: " + cur.getArea());
+            System.out.println("My perimeter is: " + cur.getPerimeter());
+        }
     }
 }
