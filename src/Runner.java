efb689ed0896dbd1ead1d8fcbd65def4e854334a
bryanttsang <43279760+bryanttsang@users.noreply.github.com>
2018-10-18 14:28:13 -0400

    Update Runner.java

--- src/Runner.java
+++ src/Runner.java
@@ -5,8 +5,9 @@ public class Runner
         Shape shape1 = new Rectangle (10, 5);
         Shape shape2 = new ETriangle(12);
         Shape shape3 = new Circle(5);
+        Shape shape4 = new Ellipse(2, 5);
 
-        Shape[] shapes = {shape1, shape2, shape3};
+        Shape[] shapes = {shape1, shape2, shape3, shape4};
 
         for (Shape cur : shapes)
         {
