58d3c8722a032474b348464fab42dc35ca7af02a
BT_1N3_20 <wtsang1931@bths.edu>
2018-10-18 14:41:30 -0400

    circle and ellipse

--- src/Circle.java
+++ src/Circle.java
@@ -2,18 +2,30 @@ public class Circle extends Shape
 {
     private int radius;
 
+    /**
+     * defines the shape to be a circle and its radius
+     * @param radius radius of the circle
+     */
     public Circle(int radius)
     {
         super("Circle");
         this.radius = radius;
     }
-    
+
+    /**
+     * finds the area of the circle
+     * @return the area of the circle
+     */
     @Override
     public double getArea()
     {
         return Math.PI * radius * radius;
     }
 
+    /**
+     * finds the circumference of the circle
+     * @return the circumference of the circle
+     */
     @Override
     public double getPerimeter()
     {
--- src/Ellipse.java
+++ src/Ellipse.java
@@ -3,19 +3,32 @@ public class Ellipse extends Shape
     private double aAxis;
     private double bAxis;
 
+    /**
+     * defines the shape to be an ellipse and the axises of it
+     * @param aAxis semi-major axis
+     * @param bAxis semi-minor axis
+     */
     public Ellipse(double aAxis, double bAxis)
     {
         super("Ellipse");
         this.aAxis = aAxis;
         this.bAxis = bAxis;
     }
-    
+
+    /**
+     * finds the area of the ellipse
+     * @return area of the ellipse
+     */
     @Override
     public double getArea()
     {
         return Math.PI * aAxis * bAxis;
     }
-    
+
+    /**
+     * finds the circumference of the ellipse
+     * @return circumference of the ellipse
+     */
     @Override
     public double getPerimeter()
     {
