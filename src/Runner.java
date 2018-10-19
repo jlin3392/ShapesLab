0aade859283938a95fc9a008d16bbc3a888cc602
bryanttsang <43279760+bryanttsang@users.noreply.github.com>
2018-10-19 14:48:26 -0400

    Update Runner.java

--- src/Runner.java
+++ src/Runner.java
@@ -2,21 +2,29 @@ public class Runner
 {
     public static void main(String[] args)
     {
-        Shape shape1 = new Rectangle (10, 5);
+        Shape shape1 = new Rectangle (10, 10);
         Shape shape2 = new ETriangle(12);
         Shape shape3 = new Circle(5);
-        Shape shape4 = new Square(6);
-        Shape shape5 = new IsoTriangle(10, 12);
-        Shape shape6 = new Ellipse(6, 7);
+        Shape shape4 = new IsoTriangle(10, 12);
+        Shape shape5 = new Ellipse(6, 7);
 
 
-        Shape[] shapes = {shape1, shape2, shape3, shape4, shape5, shape6};
+        Shape[] shapes = {shape1, shape2, shape3, shape4, shape5};
 
         for (Shape cur : shapes)
         {
-            System.out.println(cur);
-            System.out.println("My area is: " + cur.getArea());
-            System.out.println("My perimeter is: " + cur.getPerimeter());
+            if ((cur.getPerimeter() / 4) * (cur.getPerimeter() / 4) == cur.getArea())
+            {
+                System.out.println("I am a Square.");
+                System.out.println("My area is: " + cur.getArea());
+                System.out.println("My perimeter is: " + cur.getPerimeter());
+            }
+            else
+            {
+                System.out.println(cur);
+                System.out.println("My area is: " + cur.getArea());
+                System.out.println("My perimeter is: " + cur.getPerimeter());
+            }
         }
     }
 }
